bef600d4ee612bcedc148cd23ed3be4210cde1b2
Xing Wei <xing.wei@airbnb.com>
2023-11-23 17:41:51 -0800

    Set up the router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Let's dance</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+function Homepage() {
+  return <h1>Hi</h1>;
+}
 export default App;

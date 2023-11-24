a423b96bc2910f5397f352a2cdba798fac22629a
Xing Wei <xing.wei@airbnb.com>
2023-11-23 16:37:00 -0800

    Adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCiNm9RsteOqi-4dysNrq3sjS_FNk5Vh6M',
+  authDomain: 'slack-clone-4570c.firebaseapp.com',
+  projectId: 'slack-clone-4570c',
+  storageBucket: 'slack-clone-4570c.appspot.com',
+  messagingSenderId: '1059035726649',
+  appId: '1:1059035726649:web:3767ff4b363290af941ee8',
+  measurementId: 'G-NP30TYZX4T',
+};
